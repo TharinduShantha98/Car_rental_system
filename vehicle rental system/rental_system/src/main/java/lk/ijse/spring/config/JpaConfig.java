0d8c7a156bca8fb7e12fc926b81f809d238e5881
TharinduShantha98 <tharindushantha4@gmail.com>
2022-07-06 10:41:12 +0530

    jpaConfig class created

--- vehicle rental system/rental_system/src/main/java/lk/ijse/spring/config/JpaConfig.java
+++ vehicle rental system/rental_system/src/main/java/lk/ijse/spring/config/JpaConfig.java
@@ -1,16 +1,87 @@
 package lk.ijse.spring.config;
 
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "lk.ijse.spring.repo")
 @EnableTransactionManagement
 @PropertySource("classpath:application.properties")
+
+
+
+
 public class JpaConfig {
+
+
+    @Autowired
+    Environment emv;
+
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter){
+
+        LocalContainerEntityManagerFactoryBean bean =  new LocalContainerEntityManagerFactoryBean();
+        bean.setJpaVendorAdapter(jpaVendorAdapter);
+        bean.setDataSource(dataSource);
+        bean.setPackagesToScan(emv.getRequiredProperty("entity_package_name"));
+        return bean;
+
+    }
+
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource basicDataSource =  new BasicDataSource();
+        basicDataSource.setDriverClassName(emv.getRequiredProperty("my_app_driverClassName"));
+        basicDataSource.setUrl(emv.getRequiredProperty("my_app_url"));
+        basicDataSource.setUsername("my_app_user_name");
+        basicDataSource.setPassword("my_app_password");
+        basicDataSource.setMaxTotal(5);
+        basicDataSource.setInitialSize(5);
+
+        return basicDataSource;
+    }
+
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter va =  new HibernateJpaVendorAdapter();
+        va.setDatabasePlatform(emv.getRequiredProperty("my_app_dialect"));
+        va.setDatabase(Database.MYSQL);
+        va.setGenerateDdl(true);
+        va.setShowSql(true);
+        return va;
+
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
+    }
+
+
+
+
+
+
+
 
 
 
