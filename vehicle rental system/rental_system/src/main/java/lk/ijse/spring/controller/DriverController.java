1461167d2ec002e7b1671d46cc9da558d3decf05
TharinduShantha98 <tharindushantha4@gmail.com>
2022-07-07 15:00:00 +0530

    create driver controller class

--- vehicle rental system/rental_system/src/main/java/lk/ijse/spring/controller/DriverController.java
+++ vehicle rental system/rental_system/src/main/java/lk/ijse/spring/controller/DriverController.java
@@ -3,11 +3,12 @@ package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.DriverDto;
 import lk.ijse.spring.service.DriverService;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/driver")
@@ -19,14 +20,41 @@ public class DriverController {
     DriverService driverService;
 
 
-    @PostMapping
-    public void setCustomer(DriverDto driverDto){
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil setDriver(DriverDto driverDto){
         driverService.saveDriver(driverDto);
-
-
+        return  new ResponseUtil(200,"Successful added Driver for database", null);
 
     }
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllDriver(){
+        List<DriverDto> allDriver = driverService.getAllDriver();
+        return  new ResponseUtil(200,"successful get all Drivers",allDriver);
+
+    }
+
+
+    @GetMapping(value = "search", params = "id", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchDriver(@RequestParam String id){
+        DriverDto driverDto = driverService.searchDriver(id);
+        return  new ResponseUtil(200,"get all Driver successful",driverDto);
+    }
+
+    @DeleteMapping(params = "id",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteDriver(String id){
+        driverService.deleteDriver(id);
+        return new ResponseUtil(200, "delete driver successful",null);
+
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateDriver(@RequestBody DriverDto driverDto){
+        driverService.updateDriver(driverDto);
+        return  new ResponseUtil(200,"successful update driver",null);
+
+
+    }
 
 
 }
