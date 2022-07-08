4e481ee5a20c734f4ac22bbeda70872ae940e2fb
TharinduShantha98 <tharindushantha4@gmail.com>
2022-07-08 10:48:59 +0530

    created admin service

--- vehicle rental system/rental_system/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
+++ vehicle rental system/rental_system/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
@@ -1,13 +1,17 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.AdminDto;
+import lk.ijse.spring.entity.Admin;
 import lk.ijse.spring.repo.AdminRepo;
 import lk.ijse.spring.service.AdminService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -15,30 +19,80 @@ public class AdminServiceImpl implements AdminService {
 
     @Autowired
     AdminRepo adminRepo;
+    @Autowired
+    ModelMapper modelMapper;
 
 
     @Override
     public void saveAdmin(AdminDto adminDto) {
 
+        if(!adminRepo.existsById(adminDto.getAdminId())){
+            adminRepo.save(modelMapper.map(adminDto, Admin.class));
+
+        }else{
+            throw new RuntimeException("this admin Id already exits");
+        }
+
+
     }
 
     @Override
     public AdminDto searchAdmin(String id) {
-        return null;
+        if(adminRepo.existsById(id)){
+            Optional<Admin> byId = adminRepo.findById(id);
+            return  modelMapper.map(byId.get(),AdminDto.class);
+        }else{
+
+            return  null;
+        }
+
+
+
     }
 
     @Override
     public void deleteAdmin(String id) {
 
+        if(adminRepo.existsById(id)){
+            adminRepo.deleteById(id);
+
+        }else {
+            throw  new RuntimeException("this customer not found ");
+        }
+
     }
 
     @Override
     public void updateAdmin(AdminDto adminDto) {
+        if(adminRepo.existsById(adminDto.getAdminId())){
+            adminRepo.save(modelMapper.map(adminDto,Admin.class));
+
+        }else{
+            throw  new RuntimeException("this customer not found for update");
+
+        }
+
+
+
+
+
+
+
 
     }
 
     @Override
     public List<AdminDto> getAllAdmin() {
-        return null;
+
+        List<Admin> all = adminRepo.findAll();
+        List<AdminDto> allAdmins = new ArrayList<>();
+
+
+        for (Admin a: all
+             ) {
+                allAdmins.add(modelMapper.map(a, AdminDto.class));
+        }
+
+        return allAdmins;
     }
 }
