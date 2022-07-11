05e3d98715bc883fe8b2488970ce68be153965ad
TharinduShantha98 <tharindushantha4@gmail.com>
2022-07-11 20:59:49 +0530

    move

--- vehicle rental system/rental_system/src/main/java/lk/ijse/spring/controller/CarController.java
+++ vehicle rental system/rental_system/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -1,6 +1,8 @@
 package lk.ijse.spring.controller;
 
 
+import lk.ijse.spring.dto.CarDto;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,12 @@ public class CarController {
 
 
 
+    public ResponseUtil saveCar(CarDto carDto){
 
+        return  null;
+        
+
+    }
 
 
 }
